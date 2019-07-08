b4798956d6c4db16d289654bcfa29953cf502e61
AmariEllington <amari.l.ellington@gmail.com>
2019-07-08 08:32:32 +0100

    pushing so able to pull

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 import "./App.css";
 import LandingPage from "./containers/LandingPage";
 import SignInForm from "./components/LogIn";
--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
